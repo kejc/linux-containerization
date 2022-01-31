f5b78aa6aa33c829124c7811c7e00b45e553e3e2
Kenneth Clausen <me@kjc.dk>
2022-01-31 21:55:55 +0100

    Updated vuex to support localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,10 @@ export default createStore({
   state: {
   },
   mutations: {
+    initLocalStorageCache(state) {
+      if(localStorage.getItem('store'))
+        this.replaceState(Object.assign(state, JSON.parse(localStorage.getItem('store'))));
+    },
   },
   actions: {
   },
