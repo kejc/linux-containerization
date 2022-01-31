28de1a39db18db4c26c8878d3d863e6eeff23f5f
Kenneth Clausen <me@kjc.dk>
2022-01-31 21:56:06 +0100

    Implemented basic scaffolding

---
+++ src/components/ContentOverview.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside id="content-overview" class="sidebar">
+    <ul>
+      <li v-for="i in 16"
+        v-bind:key="i">
+        Example Content
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header id="site-header">
+    <h1>Linux & Containerization</h1>
+  </header>
+</template>
